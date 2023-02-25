5bb4930e455d41e1fb8b3d5c8b5752a49aff8144
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2023-02-25 12:14:43 +0100

    platformer levels twixes

--- Assets/_Project/Scripts/Platformer/PlatformerPlayer.cs
+++ Assets/_Project/Scripts/Platformer/PlatformerPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PlatformerPlayer : MonoBehaviour
 {
     public float moveSpeed;
@@ -12,17 +13,17 @@ public class PlatformerPlayer : MonoBehaviour
     public Transform GroundCheck;
     public LayerMask GroundLayer;
 
+    public AudioSource _as;
     bool isGrounded;
 
     float moveInput;
     Rigidbody2D rb2d;
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        _as = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
@@ -47,6 +48,7 @@ public class PlatformerPlayer : MonoBehaviour
             CheckIfGrounded();
             if (jumpsLeft > 0)
             {
+                _as.Play();
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
                 jumpsLeft--;
             }
