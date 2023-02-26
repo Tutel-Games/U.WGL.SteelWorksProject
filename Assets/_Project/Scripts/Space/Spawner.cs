53319d4789718325215547fa350e1031ab24092b
5Z3L4 <58202728+5Z3L4@users.noreply.github.com>
2023-02-26 12:51:07 +0100

    fix

--- Assets/_Project/Scripts/Space/Spawner.cs
+++ Assets/_Project/Scripts/Space/Spawner.cs
@@ -10,17 +10,22 @@ public class Spawner : MonoBehaviour
     [SerializeField] private List<Transform> _destinations;
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private float _timerStart = 4;
-    [SerializeField] private float _timer = 4;
+    [SerializeField] private float _timer = 0;
+    [SerializeField] private GameObject _obj;
     private Random _rnd = new();
-
-    private void Start()
-    {
-        _timer = _timerStart;
-        SpawnEnemy(0);
-    }
+    
+    private bool _shouldSpawn;
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _shouldSpawn = true;
+            _obj.SetActive(false);
+        }
+
+        if (!_shouldSpawn) return;
+        
         if (_timer > 0)
         {
             _timer -= Time.deltaTime;
